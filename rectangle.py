58dd2dc76eaf51d90481bddb34ba1defa2cd4428
lyclyq <llylcq@163.com>
2018-10-13 17:56:13 +0800

    Create rectangle.py
    试听课

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+import turtle
+colors=["white","red","blue","yellow","green","orange"]
+
+t=turtle.Pen()
+t.speed(0)
+turtle.bgcolor("black")
+for x in range(100):
+    t.pencolor(colors[x%1])
+    t.width(x/200+0.5)
+    t.forward(2*x)
+    t.left(90)
+
