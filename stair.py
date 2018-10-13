597f49f7c92158209e1d530426f11a4e211bb91e
lyclyq <llylcq@163.com>
2018-10-13 18:20:14 +0800

    stair.py
    py 3th file

---
+++ stair.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+t=turtle.Pen()
+turtle.bgcolor("black")
+t.pencolor("white")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
